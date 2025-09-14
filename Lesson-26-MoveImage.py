f085cc66a40af661efd5e2ef7cb9de69cd178a51
tyumentseva97 <tyumenceva97@gmail.com>
2025-09-14 14:40:15 +0500

    Первый коммит

---
+++ Lesson-26-MoveImage.py
@@ -0,0 +1,36 @@
+import pygame
+
+MAX_X = 800
+MAX_Y = 600
+game_over = False
+bg_color = (0, 10, 0)
+
+pygame.init()
+screen = pygame.display.set_mode((MAX_X, MAX_Y), pygame.FULLSCREEN)
+pygame.display.set_caption('My First PyGame Game! :)')
+
+myimage = pygame.image.load('unnamed.jpg').convert()
+myimage = pygame.transform.scale(myimage, (100, 100))
+
+x = 500
+y = 100
+
+while game_over == False:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                game_over = True
+            if event.key == pygame.K_LEFT:
+                x -= 20
+            if event.key == pygame.K_RIGHT:
+                x += 20
+            if event.key == pygame.K_UP:
+                y -= 20
+            if event.key == pygame.K_DOWN:
+                y += 20
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+    screen.fill(bg_color)
+    screen.blit(myimage, (x, y))
+    pygame.display.flip()
